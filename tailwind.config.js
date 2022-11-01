fafba839bb16e7256cc48edd8da7819b2a2de8e6
Xavier Brasher <xavier.brasher@gmail.com>
2022-11-01 14:05:02 +0800

    addded tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#3ABFF8',
+          secondary: '#828DF8',
+          accent: '#F471B5',
+          neutral: '#1D283A',
+          'base-100': '#0F1729',
+          info: '#0CA6E9',
+          success: '#2BD4BD',
+          warning: '#F4C152',
+          error: '#FB6F84',
+        },
+      },
+    ],
+  },
+
+  plugins: [require('daisyui')],
+}
